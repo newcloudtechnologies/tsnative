1396b108659c2a6e1fac36a5dd36ce1bd6d8d637
artem.silivanchik <artem.silivanchik@myoffice.team>
2020-09-15 06:31:17 +0300

    [#4134] Implement string tests (trim, substring, slice) (!26)
    Co-authored-by: Artem Silivanchik <artem.silivanchik@myoffice.team>
    Reviewed-on: https://dsfgit.devos.club/antiq/typescript-llvm/pulls/26
    Reviewed-by: alex.voloshanenko <alexander.voloshanenko@myoffice.team>

---
+++ test/unit/array.ts
@@ -0,0 +1,5 @@
+{
+  let ids: Array<number> = [1, 2, 3, 4, 5];
+  console.assert(ids.length === 5, "Array: failed");
+  console.assert(ids[0] === 1, "Array: failed");
+}
--- test/unit/string.ts
+++ test/unit/string.ts
@@ -1,14 +1,57 @@
-let s: string
+{
+  let s: string
 
-s = "Hello world, welcome to the universe.";
+  s = "Hello world, welcome to the universe.";
 
-console.assert(s.length === 37, "String: length failed");
+  console.assert(s.length === 37, "String: length failed");
+}
 
-console.assert(!s.startsWith("Hello"), "String: startsWith() failed");
+{
+  let s: string = "Hello world, welcome to the universe.";
 
-console.assert(!s.endsWith("universe."), "String: endsWith() failed");
+  console.assert(!s.startsWith("Hello"), "String: startsWith() failed");
+  console.assert(!s.endsWith("universe."), "String: endsWith() failed");
+}
 
-let part1 = "One";
-let part2 = "Two";
-let part12 = part1.concat(part2);
-console.assert(part12 === "OneTwo", "String: concat() failed");
+{
+  let s: string = "Hello world, welcome to the universe.";
+
+  let part1 = "One";
+  let part2 = "Two";
+  let part12 = part1.concat(part2);
+  console.assert(part12 === "OneTwo", "String: concat() failed");
+}
+
+{
+  const str = 'Transplantation';
+
+  console.assert(str.slice(5) === "plantation", "String: slice(5) failed");
+  console.assert(str.slice(-4) === "tion", "String: slice(-4) failed");
+  console.assert(str.slice(50) === "", "String: slice(50) failed");
+  console.assert(str.slice(-50) === "Transplantation", "String: slice(-50) failed");
+
+  console.assert(str.slice(5, 9) === "plan", "String: slice(5, 9) failed");
+  console.assert(str.slice(-10, -5) === "plant", "String: slice(-10, -5) failed");
+  console.assert(str.slice(-10, 10) === "plant", "String: slice(-10, 10) failed");
+}
+
+{
+  const str = 'Transplantation';
+
+  console.assert(str.substring(0) === "Transplantation", "String: substring(0) failed");
+  console.assert(str.substring(5) === "plantation", "String: substring(5) failed");
+  console.assert(str.substring(50) === "", "String: substring(50) failed");
+  console.assert(str.substring(-5) === "Transplantation", "String: substring(-5) failed");
+
+  console.assert(str.substring(0, 5) === "Trans", "String: substring(0, 5) failed");
+  console.assert(str.substring(-2, 4) === "Tran", "String: substring(-2, 4) failed");
+  console.assert(str.substring(-2, -4) === "", "String: substring(-2, -4) failed");
+  console.assert(str.substring(3, 3) === "", "String: substring(3, 3) failed");
+  console.assert(str.substring(0, 50) === "Transplantation", "String: substring(0, 50) failed");
+}
+
+{
+  const str = '\n\t\r   Transplantation  ';
+
+  console.assert(str.trim() === "Transplantation", "String: trim() failed");
+}
