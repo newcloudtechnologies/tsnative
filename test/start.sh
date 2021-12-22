e16c99060dcec3e9651d49430665d8a134d8c749
Evgeniy Makarov <eugene.makarov@myoffice.team>
2021-12-22 16:04:14 +0300

    wip

--- test/start.sh
+++ test/start.sh
@@ -17,7 +17,7 @@ BUILD_DIR="${CURRENT_DIR}/../out/test/build"
 rm -rf ${BUILD_DIR}
 mkdir -p ${BUILD_DIR}
 
-BUILD_DIR=$(readlink -f ${BUILD_DIR})
+BUILD_DIR=$(readlink ${BUILD_DIR})
 if [ "$(uname -s)" == "Darwin" ]; then
     MACOS_SYSROOT="$(xcode-select -print-path)/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk"
 fi
