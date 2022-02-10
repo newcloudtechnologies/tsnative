c9d642a74306d18983f980dbff93ce25809cd17d
Artem Silivanchik <artem.silivanchik@myoffice.team>
2022-02-10 18:38:49 +0700

    update tsstring

--- std/include/std/tsstring.h
+++ std/include/std/tsstring.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <TS.h>
+
 #include "std/private/options.h"
 
 #include "std/iterable.h"
@@ -14,59 +16,61 @@ class Number;
 
 class StringPrivate;
 
-class String : public Iterable<String*>
+class TS_EXPORT String : public Iterable<String*>
 {
 public:
-    String();
+    TS_METHOD TS_SIGNATURE("constructor(_: any)") String();
     String(Number* d);
     String(const int8_t* s);
     String(const std::string& s);
     String(const char* s);
 
-    Number* length() const;
-    String* concat(String* other) const;
+    TS_METHOD TS_GETTER Number* length() const;
+    TS_METHOD String* concat(String* other) const;
 
     Boolean* startsWith(String* other) const;
-    Boolean* startsWith(String* other, Number* startIndex) const;
+    TS_METHOD Boolean* startsWith(String* other, Number* startIndex) const;
 
     Boolean* endsWith(String* other) const;
-    Boolean* endsWith(String* other, Number* startIndex) const;
+    TS_METHOD Boolean* endsWith(String* other, Number* startIndex) const;
 
     Array<String*>* split(String* pattern) const;
-    Array<String*>* split(String* pattern, Number* limit) const;
+    TS_METHOD TS_RETURN_TYPE("string[]") Array<String*>* split(String* pattern, Number* limit) const;
 
     String* slice(Number* startIndex) const;
-    String* slice(Number* startIndex, Number* endIndex) const;
+    TS_METHOD String* slice(Number* startIndex, Number* endIndex) const;
 
     String* substring(Number* startIndex) const;
-    String* substring(Number* startIndex, Number* endIndex) const;
+    TS_METHOD String* substring(Number* startIndex, Number* endIndex) const;
 
-    String* trim() const;
+    TS_METHOD String* trim() const;
 
-    String* toLowerCase() const;
-    String* toUpperCase() const;
+    TS_METHOD String* toLowerCase() const;
+    TS_METHOD String* toUpperCase() const;
 
     Boolean* includes(String* pattern) const;
-    Boolean* includes(String* pattern, Number* startIndex) const;
+    TS_METHOD Boolean* includes(String* pattern, Number* startIndex) const;
 
     Number* indexOf(String* pattern) const;
-    Number* indexOf(String* pattern, Number* startIndex) const;
+    TS_METHOD Number* indexOf(String* pattern, Number* startIndex) const;
 
     Number* lastIndexOf(String* pattern) const;
-    Number* lastIndexOf(String* pattern, Number* startIndex) const;
+    TS_METHOD Number* lastIndexOf(String* pattern, Number* startIndex) const;
 
-    Boolean* equals(String* other) const;
+    TS_METHOD Boolean* equals(String* other) const;
+
+    String* operator+(String* other) const;
 
     String* operator[](Number* index) const;
     String* operator[](size_t index) const;
 
-    Boolean* toBool() const;
+    TS_METHOD Boolean* toBool() const;
 
     std::string cpp_str() const;
 
-    IterableIterator<String*>* iterator() override;
+    TS_METHOD TS_RETURN_TYPE("StringIterator<string>") TS_DECORATOR("MapsTo('iterator')") TS_IGNORE IterableIterator<String*>* iterator() override;
 
-    String* clone() const;
+    TS_METHOD String* clone() const;
 
     friend std::ostream& operator<<(std::ostream& os, String* s);
 
