6845bc77db017a599bc16e58a9d28b6ee9d5091b
Artem Silivanchik <artem.silivanchik@myoffice.team>
2022-03-11 13:42:18 +0700

    TARGET_ABI detection

--- tsnative.sh
+++ tsnative.sh
@@ -160,11 +160,27 @@ then
     PRINT_IR=FALSE
 fi
 
-# FIXME: make it configurable
-# if [ -z "$TARGET_ABI" ]
-# then
-#     TARGET_ABI="x86_64-linux-gnu"
-# fi
+OS=$(uname -s)
+ARCH=$(uname -m)
+
+# TODO: support Android and iOS
+
+if [[ $OS == Linux ]]; then
+    case "$ARCH" in
+        i?86) TARGET_ABI="i686-linux-gnu" ;;
+        x86_64) TARGET_ABI="x86_64-linux-gnu" ;;
+    esac
+elif [[ $OS == MINGW* ]]; then
+    case "$ARCH" in
+        i?86) TARGET_ABI="i686-w64-mingw32" ;;
+        x86_64) TARGET_ABI="x86_64-w64-mingw32" ;;
+    esac
+else
+    echo "Unsupported OS"
+    exit 1;
+fi
+
+echo "TARGET_ABI detected: ${TARGET_ABI}"
 
 if [ "$(uname -s)" == "Darwin" ]; then
     JOBS_NUM=$(sysctl -n hw.ncpu)
