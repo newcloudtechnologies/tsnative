c6c8df5b126373ea30b3f825ea8a259546a5b8de
Artem Silivanchik <artem.silivanchik@myoffice.team>
2022-03-16 14:55:17 +0700

    add TSClosure

--- std/include/std/tsclosure.h
+++ std/include/std/tsclosure.h
@@ -1,24 +1,29 @@
 #pragma once
 
+#include <TS.h>
+
 #include "std/private/options.h"
 
 #include "std/tsboolean.h"
 #include "std/tsnumber.h"
 #include "std/tsoptional.h"
 
+TS_CODE("// @ts-ignore\n"
+        "export type TSClosure = Function;\n");
+        
 class TSClosure
 {
 public:
-    TSClosure(void* fn, void** env, Number* numArgs, Number* optionals);
+    TS_METHOD TSClosure(void* fn, void** env, Number* numArgs, Number* optionals);
 
-    void** getEnvironment() const;
+    TS_METHOD void** getEnvironment() const;
 
     template <typename T>
     void setEnvironmentElement(T value, int index);
 
     Number* getNumArgs() const;
 
-    void* operator()();
+    TS_METHOD TS_SIGNATURE("call(): void") TS_DECORATOR("MapsTo('operator()()')") TS_IGNORE void* operator()();
 
 private:
     void* fn = nullptr;
