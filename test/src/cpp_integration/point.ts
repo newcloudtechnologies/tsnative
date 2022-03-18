48fe7ac5ccfef5c25b0b19f883337292e76d550a
Artem Silivanchik <artem.silivanchik@myoffice.team>
2022-03-18 13:38:39 +0700

    debug tests: point

--- test/src/cpp_integration/point.ts
+++ test/src/cpp_integration/point.ts
@@ -4,6 +4,7 @@ const p1 = new Point(1, 1);
 console.assert(p1.x() === 1, "Point 'x' test failed");
 console.assert(p1.x() === p1.y(), "Point 'x' & 'y' test failed");
 
+
 const p2 = p1.clone();
 console.assert(p2.x() === 1, "Point.clone 'x' test failed");
 console.assert(p2.x() === p2.y(), "Point.clone 'x' & 'y' test failed");
@@ -19,10 +20,13 @@ console.assert(p2.x() !== p2.y(), "Point x/y not equals test failed");
 console.assert(p1 !== p2, "C++ objects comparison test failed");
 
 const rect0 = new Rect(p1, p2);
+/*
 const rectDiagonal = rect0.getDiagonal();
+
 console.assert(rectDiagonal.length === 2, "Point array providing test #1 failed");
 
 function isPointEqual(rhs: Point, lhs: Point) {
   return (rhs.x() === lhs.x()) && (rhs.y() === lhs.y());
 }
 console.assert(isPointEqual(rectDiagonal[0], p1) && isPointEqual(rectDiagonal[1], p2), "Point array providing test #2 failed");
+*/
