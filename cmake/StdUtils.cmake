f169212eb70c3fd6e3082f5b51d5f2686271aa3c
Artem Silivanchik <artem.silivanchik@myoffice.team>
2022-03-21 12:11:54 +0700

    add StdUtils

---
+++ cmake/StdUtils.cmake
@@ -0,0 +1,19 @@
+#
+# Copyright (c) New Cloud Technologies, Ltd., 2014-2021
+# 
+# You can not use the contents of the file in any way without
+# New Cloud Technologies, Ltd. written permission.
+# 
+# To obtain such a permit, you should contact New Cloud Technologies, Ltd.
+# at http://ncloudtech.com/contact.html
+#
+
+set(STD_DEFINITIONS 
+    USE_STD_ARRAY_BACKEND
+    USE_MAP_STD_BACKEND
+    USE_SET_STD_BACKEND
+    USE_TUPLE_STD_BACKEND
+    USE_BOOLEAN_CXX_BUILTIN_BACKEND
+    USE_NUMBER_CXX_BUILTIN_BACKEND
+    USE_STD_STRING_BACKEND
+)
