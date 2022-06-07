81382bf88f541994621b01b9219a88f1f4c4e5ec
andrey.tutyne <andrey.tutyne@myoffice.team>
2022-06-07 17:05:56 +0300

    [AN-1019] Rework exceptions handling (!261)
    Reviewed-on: https://gitea.devos.club/antiq/tsnative/pulls/261
    Reviewed-by: alex.voloshanenko <alexander.voloshanenko@myoffice.team>
    Reviewed-by: eugene.makarov <eugene.makarov@myoffice.team>

--- compiler/src/handlers/node/exceptionhandler.ts
+++ compiler/src/handlers/node/exceptionhandler.ts
@@ -13,10 +13,17 @@ import * as ts from "typescript";
 import { AbstractNodeHandler } from "./nodehandler";
 import { Scope, Environment } from "../../scope";
 import * as llvm from "llvm-node";
-import { LLVMConstant, LLVMConstantInt, LLVMGlobalVariable, LLVMValue } from "../../llvm/value";
+import {
+  LLVMConstant,
+  LLVMConstantInt,
+  LLVMGlobalVariable,
+  LLVMValue,
+} from "../../llvm/value";
 import { LLVMFunction } from "../../llvm/function";
 import { LLVMType } from "../../llvm/type";
 import { getInvocableBody, needUnwind } from "../../builder/builder";
+
+type FunctionEntry = { fn: LLVMValue; existing: boolean };
 
 /*
   ExceptionHandler class is provides exception handling via C++ Runtime realization described
@@ -25,26 +32,34 @@ import { getInvocableBody, needUnwind } from "../../builder/builder";
 */
 
 export class ExceptionHandler extends AbstractNodeHandler {
-  private static ehIrGenerator: llvm.ExceptionHandlingGenerator; // C++ wrapper for using in here
   private static typeInfo: LLVMGlobalVariable; // Type-info uses C++ RTTI. Store type information in runtime
-  private static allocateExceptionFn: LLVMValue; // __cxa_allocate_exception --- LLVM ABI Itanium intrinsic
-  private static throwExceptionFn: LLVMValue; // __cxa_throw --- LLVM ABI Itanium intrinsic
-  private static endCatchFn: LLVMValue; // __cxa_end_catch --- LLVM ABI Itanium intrinsic
+  private static allocateException: FunctionEntry; // __cxa_allocate_exception --- LLVM ABI Itanium intrinsic
+  private static throwException: FunctionEntry; // __cxa_throw --- LLVM ABI Itanium intrinsic
+  private static beginCatch: FunctionEntry; // __cxa_begin_catch --- LLVM ABI Itanium intrinsic
+  private static endCatch: FunctionEntry; // __cxa_end_catch --- LLVM ABI Itanium intrinsic
+  private static personality: FunctionEntry; // __gxx_personality_v0 -- Personality function
   private readonly unreachableBlockName: string = "unreachable";
 
   handle(node: ts.Node, parentScope: Scope, env?: Environment): boolean {
     const { builder, currentFunction, context } = this.generator;
-    this.initGeneratorForException();
+    this.initRTTI();
     if (ts.isTryStatement(node)) {
       // Emit all declaration and type info
       // Mark attr function as required unwinding
       currentFunction.addFnAttr(llvm.Attribute.AttrKind.UWTable);
 
       // Set LSDA handler personality. This suggests that the function will language specific have an unwinder(C++)
-      ExceptionHandler.ehIrGenerator.setPersonalityFn(currentFunction);
+      currentFunction.setPersonalityFn(
+        ExceptionHandler.personality.fn.unwrapped as llvm.Constant
+      );
+      // ExceptionHandler.ehIrGenerator.setPersonalityFn(currentFunction);
 
-      const tryBlockBB = llvm.BasicBlock.create(context, "try", currentFunction);
-      const ldPadBB = llvm.BasicBlock.create(context, "lpad", currentFunction);
+      const tryBlockBB = llvm.BasicBlock.create(
+        context,
+        "try",
+        currentFunction
+      );
+      const ldPadBB = llvm.BasicBlock.create(context, "catch.body", currentFunction);
 
       /* We will keep a stack of landing pages in order to correctly handle
          the context of the transition to the unwinding instructions
@@ -54,9 +69,16 @@ export class ExceptionHandler extends AbstractNodeHandler {
       this.generator.builder.createBr(tryBlockBB);
       this.generator.builder.setInsertionPoint(tryBlockBB);
       this.generator.handleNode(node.tryBlock, parentScope, env);
-      const afterTryBB = llvm.BasicBlock.create(context, "after.try", currentFunction);
+      const afterTryBB = llvm.BasicBlock.create(
+        context,
+        "after.try",
+        currentFunction
+      );
       const hasTerminator = Boolean(builder.getInsertBlock()?.getTerminator());
-      if (!builder.getInsertBlock()?.name.startsWith("throw") && !hasTerminator) {
+      if (
+        !builder.getInsertBlock()?.name.startsWith("throw") &&
+        !hasTerminator
+      ) {
         builder.createBr(afterTryBB);
       }
 
@@ -85,17 +107,46 @@ export class ExceptionHandler extends AbstractNodeHandler {
       const lpad = builder.landingPadStack[builder.landingPadStack.length - 1];
       builder.landingPadStack.pop();
       this.generator.builder.setInsertionPoint(lpad);
-      const alloca = this.generator.builder.unwrap().createAlloca(llvm.Type.getInt8PtrTy(this.generator.context));
-      ExceptionHandler.ehIrGenerator.addLandingPad(alloca);
+      const int8PtrTy = LLVMType.getInt8Type(this.generator).getPointer();
+      const int32Ty = LLVMType.getInt32Type(this.generator);
+
+      const structType = llvm.StructType.get(context, [
+        int8PtrTy.type,
+        int32Ty.type,
+      ]);
+      const exception = builder
+        .unwrap()
+        .CreateLandingPad(structType, 1, "exception.inst");
+
+      // Always only one catch block with type.
+      exception.addClause(
+        llvm.ConstantExpr.getBitCast(
+          ExceptionHandler.typeInfo.unwrapped as llvm.Constant,
+          int8PtrTy.type,
+          false
+        )
+      );
+      // This flag will not call destructors for the caught value in runtime C++
+      exception.setCleanup(false);
+      const selector = builder.createSafeExtractValue(
+        LLVMValue.create(exception, this.generator),
+        [0],
+        "exception.slot"
+      );
+      const exceptionValue = builder.createSafeCall(
+        ExceptionHandler.beginCatch.fn,
+        [selector],
+        "begin_catch"
+      );
+
       if (node.variableDeclaration) {
-        const load = builder.createLoad(LLVMValue.create(alloca, this.generator));
-        parentScope.set(node.variableDeclaration.name.getText(), load);
+        parentScope.set(node.variableDeclaration.name.getText(), exceptionValue);
       }
       this.generator.handleNode(node.block, parentScope, env);
 
       const currentBB = this.generator.builder.getInsertBlock();
       if (currentBB && !currentBB.getTerminator()) {
-        this.generator.builder.createSafeCall(ExceptionHandler.endCatchFn, []);
+        this.generator.builder.createSafeCall(ExceptionHandler.endCatch.fn, []);
       }
       return true;
     }
@@ -107,82 +158,131 @@ export class ExceptionHandler extends AbstractNodeHandler {
     const int8PtrTy = LLVMType.getInt8Type(this.generator).getPointer();
 
     // Get exception payload and store to exception ptr
-    const throwBlock = llvm.BasicBlock.create(this.generator.context, "throw", currentFunction);
+    const throwBlock = llvm.BasicBlock.create(
+      this.generator.context,
+      "throw",
+      currentFunction
+    );
     builder.createBr(throwBlock);
     builder.setInsertionPoint(throwBlock);
-    const exceptionSize = LLVMConstantInt.get(this.generator, raisedException.type.getTypeSize(), 64);
+    const exceptionSize = LLVMConstantInt.get(
+      this.generator,
+      raisedException.type.getTypeSize(),
+      64
+    );
     const allocateException = builder.createSafeCall(
-      ExceptionHandler.allocateExceptionFn,
+      ExceptionHandler.allocateException.fn,
       [exceptionSize],
       "alloc.exception.ptr"
     );
-    const exceptionPtr = builder.createBitCast(allocateException, raisedException.type.getPointer());
+    const exceptionPtr = builder.createBitCast(
+      allocateException,
+      raisedException.type.getPointer()
+    );
     builder.createSafeStore(raisedException, exceptionPtr);
 
     // Income from try block.
     if (needUnwind(node)) {
-      const ldPadBB = builder.landingPadStack[builder.landingPadStack.length - 1];
+      const ldPadBB =
+        builder.landingPadStack[builder.landingPadStack.length - 1];
       const unreachableBB = this.getOrCreateUnreachableBlock();
       {
-        ExceptionHandler.ehIrGenerator.createInvoke(
-          ExceptionHandler.throwExceptionFn.unwrapped,
-          unreachableBB,
-          ldPadBB,
-          [
-            allocateException.unwrapped,
-            builder.createBitCast(ExceptionHandler.typeInfo, int8PtrTy).unwrapped,
-            LLVMConstant.createNullValue(int8PtrTy, this.generator).unwrapped,
-          ]
-        );
+        builder
+          .unwrap()
+          .CreateInvoke(
+            ExceptionHandler.throwException.fn.unwrapped,
+            unreachableBB,
+            ldPadBB,
+            [
+              allocateException.unwrapped,
+              builder.createBitCast(ExceptionHandler.typeInfo, int8PtrTy)
+                .unwrapped,
+              LLVMConstant.createNullValue(int8PtrTy, this.generator).unwrapped,
+            ]
+          );
       }
     } else {
       const block = getInvocableBody(node);
       builder.functionMetaEntry.set(block as ts.Block, { needUnwind: true });
 
       currentFunction.addFnAttr(llvm.Attribute.AttrKind.UWTable);
-      builder.createSafeCall(ExceptionHandler.throwExceptionFn, [
+      builder.createSafeCall(ExceptionHandler.throwException.fn, [
         allocateException,
         builder.createBitCast(ExceptionHandler.typeInfo, int8PtrTy),
         LLVMConstant.createNullValue(int8PtrTy, this.generator),
       ]);
-      ExceptionHandler.ehIrGenerator.createUnreachable();
+      builder.unwrap().CreateUnreachable();
     }
   }
 
-  createFunction(ret: LLVMType, args: LLVMType[], name: string): LLVMValue {
+  createFunction(
+    ret: LLVMType,
+    args: LLVMType[],
+    name: string
+  ): { fn: LLVMValue; existing: boolean } {
     const functionInstance = new LLVMFunction(this.generator);
-    return functionInstance.create(ret, args, name).fn;
+    return functionInstance.create(ret, args, name);
   }
 
   getOrCreateUnreachableBlock(): llvm.BasicBlock {
     const { context, currentFunction, builder } = this.generator;
     const blocks = currentFunction.getBasicBlocks();
-    const unreachableBB = blocks.find((block) => block.name === this.unreachableBlockName);
+    const unreachableBB = blocks.find(
+      (block: llvm.BasicBlock) => block.name === this.unreachableBlockName
+    );
     if (unreachableBB) return unreachableBB;
     return this.generator.withInsertBlockKeeping(() => {
-      const newUnreachableBB = llvm.BasicBlock.create(context, this.unreachableBlockName, currentFunction);
+      const newUnreachableBB = llvm.BasicBlock.create(
+        context,
+        this.unreachableBlockName,
+        currentFunction
+      );
       builder.setInsertionPoint(newUnreachableBB);
-      ExceptionHandler.ehIrGenerator.createUnreachable();
+      builder.unwrap().CreateUnreachable();
       return newUnreachableBB;
     });
   }
 
-  initGeneratorForException(): void {
-    if (!ExceptionHandler.ehIrGenerator) {
-      const int8PtrTy = LLVMType.getInt8Type(this.generator).getPointer();
-      const int64Ty = LLVMType.getInt64Type(this.generator);
-      const voidTy = LLVMType.getVoidType(this.generator);
+  initRTTI(): void {
+    const int8PtrTy = LLVMType.getInt8Type(this.generator).getPointer();
+    const int32Ty = LLVMType.getInt32Type(this.generator);
+    const int64Ty = LLVMType.getInt64Type(this.generator);
+    const voidTy = LLVMType.getVoidType(this.generator);
 
-      ExceptionHandler.ehIrGenerator = new llvm.ExceptionHandlingGenerator(
-        this.generator.module,
-        this.generator.builder.unwrap()
+    // Emit exception handling rtti intrinsic
+    if (!this.generator.module.getGlobalVariable("_ZTIPv")) {
+      ExceptionHandler.typeInfo = LLVMGlobalVariable.make(
+        this.generator,
+        int8PtrTy,
+        true,
+        undefined,
+        "_ZTIPv"
       );
-      // Emit exception handling rtti intrinsic
-      ExceptionHandler.typeInfo = LLVMGlobalVariable.make(this.generator, int8PtrTy, true, undefined, "_ZTIPv");
-      ExceptionHandler.allocateExceptionFn = this.createFunction(int8PtrTy, [int64Ty], "__cxa_allocate_exception");
-      ExceptionHandler.throwExceptionFn = this.createFunction(voidTy, [int8PtrTy, int8PtrTy, int8PtrTy], "__cxa_throw");
-      ExceptionHandler.endCatchFn = this.createFunction(voidTy, [], "__cxa_end_catch");
-      this.createFunction(int8PtrTy, [int8PtrTy], "__cxa_begin_catch");
     }
+    ExceptionHandler.personality = this.createFunction(
+      int32Ty,
+      [],
+      "__gxx_personality_v0"
+    );
+    ExceptionHandler.allocateException = this.createFunction(
+      int8PtrTy,
+      [int64Ty],
+      "__cxa_allocate_exception"
+    );
+    ExceptionHandler.throwException = this.createFunction(
+      voidTy,
+      [int8PtrTy, int8PtrTy, int8PtrTy],
+      "__cxa_throw"
+    );
+    ExceptionHandler.beginCatch = this.createFunction(
+      int8PtrTy,
+      [int8PtrTy],
+      "__cxa_begin_catch"
+    );
+    ExceptionHandler.endCatch = this.createFunction(
+      voidTy,
+      [],
+      "__cxa_end_catch"
+    );
   }
 }
