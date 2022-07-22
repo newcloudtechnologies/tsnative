074fdb5588308b18744a3a951f5e6d5c9a04da36
eugene.makarov <eugene.makarov@myoffice.team>
2022-07-22 14:22:02 +0300

    [AN-1067] Workaround identifier clash with property access expression name (!288)
    Co-authored-by: Evgeniy Makarov <eugene.makarov@myoffice.team>
    Reviewed-on: https://gitea.devos.club/antiq/tsnative/pulls/288
    Reviewed-by: vladimir.tsyshnatiy <vladimir.tsyshnatiy@myoffice.team>

--- compiler/src/handlers/node/functiondeclarationhandler.ts
+++ compiler/src/handlers/node/functiondeclarationhandler.ts
@@ -23,100 +23,7 @@ export class FunctionDeclarationHandler extends AbstractNodeHandler {
     handle(node: ts.Node, parentScope: Scope, outerEnv?: Environment): boolean {
         if (ts.isFunctionDeclaration(node)) {
             const declaration = Declaration.create(node, this.generator);
-
-            if (!declaration.body) {
-                return true;
-            }
-
-            const name = declaration.name?.getText();
-            if (!name) {
-                throw new Error(`Anonymous function declarations are not supported. Error at: '${declaration.getText()}'`);
-            }
-
-            const signature = this.generator.ts.checker.getSignatureFromDeclaration(declaration);
-            const parameters = signature.getDeclaredParameters();
-
-            const llvmArgumentTypes: LLVMType[] = [];
-            if (!declaration.typeParameters) {
-                const tsArgumentTypes = parameters.map((parameter) => this.generator.ts.checker.getTypeAtLocation(parameter));
-                llvmArgumentTypes.push(
-                    ...tsArgumentTypes.map((argType) => {
-                        return argType.getLLVMType();
-                    })
-                );
-            } else {
-                llvmArgumentTypes.push(...new Array(parameters.length).fill(LLVMType.getInt8Type(this.generator).getPointer()));
-            }
-
-            const scope = declaration.getScope(undefined);
-
-            // these dummy arguments will be substituted by actual arguments once called
-            const dummyArguments = llvmArgumentTypes.map((t) => {
-                let nullArg = LLVMConstant.createNullValue(t.unwrapPointer(), this.generator);
-
-                const allocated = this.generator.gc.allocate(nullArg.type.unwrapPointer());
-                this.generator.builder.createSafeStore(nullArg, allocated);
-                nullArg = allocated;
-
-                if (nullArg.type.isUnion()) {
-                    nullArg = this.generator.ts.union.create();
-                }
-
-                return nullArg;
-            });
-
-            this.generator.symbolTable.currentScope.initializeVariablesAndFunctionDeclarations(declaration.body, this.generator);
-
-            // @todo: 'this' is bindable by 'bind', 'call', 'apply' so it should be stored somewhere
-            const environmentVariables = ConciseBody.create(declaration.body, this.generator).getEnvironmentVariables(
-                signature,
-                scope,
-                outerEnv
-            );
-
-            const env = createEnvironment(
-                scope,
-                environmentVariables,
-                this.generator,
-                {
-                    args: dummyArguments,
-                    signature,
-                },
-                outerEnv
-            );
-
-            let closure: LLVMValue;
-
-            if (declaration.typeParameters) {
-                this.generator.meta.registerFunctionEnvironment(declaration, env);
-                closure = this.generator.tsclosure.lazyClosure.create(env.typed);
-            } else {
-                const tsReturnType = signature.getReturnType();
-                const llvmReturnType = tsReturnType.getLLVMReturnType();
-
-                const functionName = declaration.name
-                    ? declaration.name.getText() + "__" + this.generator.randomString
-                    : this.generator.randomString;
-                const { fn } = this.generator.llvm.function.create(llvmReturnType, [env.voidStar], functionName);
-
-                FunctionHandler.handleFunctionBody(declaration, fn, this.generator, env);
-                LLVMFunction.verify(fn, declaration);
-
-                closure = this.generator.tsclosure.createClosure(fn, env.untyped, declaration);
-            }
-
-            let existing = parentScope.get(name);
-
-            if (existing && existing instanceof LLVMValue) {
-                // overwrite pointers that possibly captured in some environments
-                existing.makeAssignment(closure);
-                // overwrite value for future uses
-                parentScope.overwrite(name, new HeapVariableDeclaration(closure, closure, name));
-            } else {
-                parentScope.set(name, new HeapVariableDeclaration(closure, closure, name));
-            }
-
-            return true;
+            return this.handleFunctionDeclaration(declaration, parentScope, outerEnv);
         }
 
         if (this.next) {
@@ -124,5 +31,118 @@ export class FunctionDeclarationHandler extends AbstractNodeHandler {
         }
 
         return false;
+    }
+
+    handleFunctionDeclaration(declaration: Declaration, parentScope: Scope, outerEnv?: Environment) {
+        if (declaration.isAmbient()) {
+            // ambient declarations are used to map calls to cxx backend, nothing to handle
+            return true;
+        }
+
+        const env = this.createEnvironmentForDeclaration(declaration, outerEnv);
+        const closure = this.createClosureForDeclaration(declaration, env);
+
+        this.registerClosureForDeclaration(closure, declaration, parentScope);
+
+        return true;
+    }
+
+    private getDeclarationName(declaration: Declaration) {
+        const name = declaration.name?.getText();
+        if (!name) {
+            throw new Error(`Anonymous function declarations are not supported. Error at: '${declaration.getText()}'`);
+        }
+
+        return name;
+    }
+
+    createEnvironmentForDeclaration(declaration: Declaration, outerEnv?: Environment) {
+        if (!declaration.body) {
+            throw new Error(`Expected body at function declaration '${declaration.getText()}'`);
+        }
+
+        const signature = this.generator.ts.checker.getSignatureFromDeclaration(declaration);
+        const parameters = signature.getDeclaredParameters();
+
+        const llvmArgumentTypes: LLVMType[] = [];
+        if (!declaration.typeParameters) {
+            const tsArgumentTypes = parameters.map((parameter) => this.generator.ts.checker.getTypeAtLocation(parameter));
+            llvmArgumentTypes.push(
+                ...tsArgumentTypes.map((argType) => {
+                    return argType.getLLVMType();
+                })
+            );
+        } else {
+            llvmArgumentTypes.push(...new Array(parameters.length).fill(LLVMType.getInt8Type(this.generator).getPointer()));
+        }
+
+        // these dummy arguments will be substituted by actual arguments once called
+        const dummyArguments = llvmArgumentTypes.map((type) => {
+            if (type.isUnion()) {
+                return this.generator.ts.union.create();
+            }
+
+            const nonPtrType = type.unwrapPointer();
+
+            const nullValue = LLVMConstant.createNullValue(nonPtrType, this.generator);
+            const allocated = this.generator.gc.allocate(nonPtrType);
+            this.generator.builder.createSafeStore(nullValue, allocated);
+
+            return allocated;
+        });
+
+        this.generator.symbolTable.currentScope.initializeVariablesAndFunctionDeclarations(declaration.body, this.generator);
+
+        const scope = this.generator.symbolTable.currentScope;
+
+        const environmentVariables = ConciseBody.create(declaration.body, this.generator).getEnvironmentVariables(
+            signature,
+            scope,
+            outerEnv
+        );
+
+        return createEnvironment(
+            scope,
+            environmentVariables,
+            this.generator,
+            {
+                args: dummyArguments,
+                signature,
+            },
+            outerEnv
+        );
+    }
+
+    createClosureForDeclaration(declaration: Declaration, env: Environment) {
+        if (declaration.typeParameters) {
+            this.generator.meta.registerFunctionEnvironment(declaration, env);
+            return this.generator.tsclosure.lazyClosure.create(env.typed);
+        }
+
+        const signature = this.generator.ts.checker.getSignatureFromDeclaration(declaration);
+        const tsReturnType = signature.getReturnType();
+        const llvmReturnType = tsReturnType.getLLVMReturnType();
+
+        const functionName = this.getDeclarationName(declaration) + "__" + this.generator.randomString;
+        const { fn } = this.generator.llvm.function.create(llvmReturnType, [env.voidStar], functionName);
+
+        FunctionHandler.handleFunctionBody(declaration, fn, this.generator, env);
+        LLVMFunction.verify(fn, declaration);
+
+        return this.generator.tsclosure.createClosure(fn, env.untyped, declaration);
+    }
+
+    registerClosureForDeclaration(closure: LLVMValue, declaration: Declaration, parentScope: Scope) {
+        const name = this.getDeclarationName(declaration);
+        const existing = parentScope.get(name);
+
+        if (existing && existing instanceof LLVMValue) {
+            // overwrite pointers that possibly captured in some environments
+            existing.makeAssignment(closure);
+            // overwrite value for future uses
+            parentScope.overwrite(name, new HeapVariableDeclaration(closure, closure, name));
+        } else {
+            parentScope.set(name, new HeapVariableDeclaration(closure, closure, name));
+        }
     }
 }
---
+++ test/src/complex/clash_with_property_access_function_import.ts
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) New Cloud Technologies, Ltd., 2014-2022
+ *
+ * You can not use the contents of the file in any way without
+ * New Cloud Technologies, Ltd. written permission.
+ *
+ * To obtain such a permit, you should contact New Cloud Technologies, Ltd.
+ * at http://ncloudtech.com/contact.html
+ *
+ */
+
+import { Documents } from "./clash_with_property_access_class_import";
+
+export function TextDocPageGenerator() {
+    return new Documents.TextDocPageGenerator();
+}
