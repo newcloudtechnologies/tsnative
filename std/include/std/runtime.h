238784d968a1cce6f282ebcba82d093471a032f5
Tsyshnatiy <vladimir.tsyshnatiy@myoffice.team>
2022-08-05 13:25:22 +0300

    Adds GC, Diagnostics, MemDiagnostics object wrappers

--- std/include/std/runtime.h
+++ std/include/std/runtime.h
@@ -17,19 +17,27 @@ template<typename T>
 class Array;
 
 class String;
+class Allocator;
+class MemoryDiagnosticsStorage;
+class IGCImpl;
 
 class TS_EXPORT TS_DECLARE Runtime final : public Object
 {
 public:
     static int init(int argc, char* argv[]);
     static void destroy();
+    static bool isInitialized();
 
     static TS_METHOD GC* getGC();
-    static void setGC(std::unique_ptr<GC> newGC);
 
     static TS_METHOD Diagnostics* getDiagnostics();
 
     static TS_METHOD Array<String*>* getCmdArgs();
+
+    // Should be removed from here
+    // Avoids recursive operator new call in tsobject.cpp->operator new
+    // Recursion is caused by the inheritance GC : Object
+    static void* allocateObject(std::size_t n);
 
     TS_METHOD String* toString() const override;
     TS_METHOD Boolean* toBool() const override;
@@ -43,8 +51,9 @@ private:
     static std::vector<std::string> _cmdArgs;
     static bool _isInitialized;
 
-    static std::unique_ptr<Diagnostics> _diagnostics;
-    static std::unique_ptr<GC> _gc;
+    static std::unique_ptr<MemoryDiagnosticsStorage> _memoryDiagnosticsStorage;
+    static std::unique_ptr<IGCImpl> _gcImpl;
+    static std::unique_ptr<Allocator> _allocator;
 };
 
 inline std::ostream& operator<<(std::ostream& os, const Runtime* runtime)
