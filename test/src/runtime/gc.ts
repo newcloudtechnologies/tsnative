84ef5cc0e4d0290e47c3f008ffc1cb4a9ce2c106
Tsyshnatiy <vladimir.tsyshnatiy@myoffice.team>
2022-08-12 14:50:00 +0300

    Adds test to collect non-primitive type

--- test/src/runtime/gc.ts
+++ test/src/runtime/gc.ts
@@ -1,25 +1,26 @@
 import { Runtime } from "tsnative/std/definitions/runtime"
 
-// // String scoped allocation
-// {
-//     // All diagnostics mechanics is created using variables to force GC not to delete it before the time comes
-//     // old and new object counts will not be equivalent otherwise becase diagnostics object will be collected
-//     const diagnostics = Runtime.getDiagnostics();
-//     const memInfo = diagnostics.getMemoryDiagnostics();
-//     const internalObjectsCount = memInfo.getAliveObjectsCount();
-//     {
-//         const myStr : string = "abacaba";
-//         myStr.trim(); // Use this memory to prove it is alive
-//     }
+// String scoped allocation
+{
+    // All diagnostics mechanics is created using variables to force GC not to delete it before the time comes
+    // old and new object counts will not be equivalent otherwise becase diagnostics object will be collected
+    const diagnostics = Runtime.getDiagnostics();
+    const memInfo = diagnostics.getMemoryDiagnostics();
+    const internalObjectsCount = memInfo.getAliveObjectsCount();
 
-//     Runtime.getGC().collect();
+    {
+        const myStr : string = "abacaba";
+        myStr.trim(); // Use this memory to prove it is alive
+    }
 
-//     const newObjectCount = memInfo.getAliveObjectsCount();
-//     console.assert(internalObjectsCount === newObjectCount, "GC failed: not all object were collected");
-// }
+    Runtime.getGC().collect();
 
-// // Collect from the prev test
-// Runtime.getGC().collect();
+    const newObjectCount = memInfo.getAliveObjectsCount();
+    console.assert(internalObjectsCount === newObjectCount, "GC failed: not all object were collected");
+}
+
+// Collect from the prev test
+Runtime.getGC().collect();
 
 // Non primitive type scoped collection
 {
@@ -29,33 +30,30 @@ import { Runtime } from "tsnative/std/definitions/runtime"
     const memInfo = diagnostics.getMemoryDiagnostics();
     const internalObjectsCount = memInfo.getAliveObjectsCount();
 
-    console.log("Scope opened");
     {
         class A {
-            // n: number;
+            n: number;
 
-            // constructor() {
-            //     this.a = 15;
-            // }
+            constructor() {
+                this.n = 15;
+            }
 
-            //get foo() { return 10; }
+            get getter() { return 10; }
 
             foo() {}
         };
         const a = new A();
         a.foo();
-        //let b = a.foo;
-        //b = 15;
-        //a.n = 10;
+
+        let b = a.getter;
+        b = 15;
+
+        a.n = 10;
     }
-    console.log("Scope closed");
 
     Runtime.getGC().collect();
     
     const newObjectCount = memInfo.getAliveObjectsCount();
-
-    // console.log(internalObjectsCount);
-    // console.log(newObjectCount);
     console.assert(internalObjectsCount === newObjectCount, "GC failed: not all object were collected");
 }
 
