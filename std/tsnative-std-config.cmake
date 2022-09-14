6a8fa075aa33066aef9b96d4920c5854a0d958b9
evgeniy.generalov <evgeniy.generalov@myoffice.team>
2022-09-14 12:19:30 +0300

    [std] Use project's cmake config (!320)
    When Conan generates configs for a dependency it creates imported interface not a library. This doesn't allow us to use neither cmake generator expressions nor read properties on imported target.
    
    Reviewed-on: https://gitea.devos.club/antiq/tsnative/pulls/320
    Reviewed-by: vladimir.tsyshnatiy <vladimir.tsyshnatiy@myoffice.team>
    Reviewed-by: artem.silivanchik <artem.silivanchik@myoffice.team>

--- std/tsnative-std-config.cmake
+++
@@ -1,6 +0,0 @@
-message(STATUS "Found tsnative-std")
-
-include(CMakeFindDependencyMacro)
-find_dependency(absl REQUIRED)
-
-include ("${CMAKE_CURRENT_LIST_DIR}/tsnative-std-targets.cmake")
---
+++ std/tsnative-stdConfig.cmake
@@ -0,0 +1,31 @@
+message(STATUS "Found tsnative-std")
+
+include(CMakeFindDependencyMacro)
+find_dependency(absl REQUIRED)
+
+include ("${CMAKE_CURRENT_LIST_DIR}/tsnative-stdTargets.cmake")
+
+# We assume this install path _PACKAGE_LIBDIR/cmake/tsnative-std/this_file
+get_filename_component(_PACKAGE_LIBDIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+get_filename_component(_PACKAGE_LIBDIR "${_PACKAGE_LIBDIR}" PATH)
+get_filename_component(_PACKAGE_LIBDIR "${_PACKAGE_LIBDIR}" PATH)
+
+find_library(tsnative-std_LIBRARY
+    NAMES tsnative-std
+    PATHS ${_PACKAGE_LIBDIR}
+    NO_DEFAULT_PATH
+    NO_CMAKE_FIND_ROOT_PATH
+)
+
+# Cleanup
+set(_PACKAGE_LIBDIR )
+
+if (NOT DEFINED tsnative-std_LIBRARY)
+    message(FATAL_ERROR "Cannot find tsnative-std library")
+else()
+    message(STATUS "Found tsnative-std library at ${tsnative-std_LIBRARY}")
+    # Some projects depends on these variables
+    list(PREPEND tsnative-std_LIBS ${tsnative-std_LIBRARY})
+    list(PREPEND tsnative-std_LIBRARIES ${tsnative-std_LIBRARY})
+endif()
+
