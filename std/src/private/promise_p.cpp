7388ed1790f35961f6ee72170af6e3ab099d619d
andrey.tutyne <andrey.tutyne@myoffice.team>
2022-10-17 13:16:06 +0300

    [TSN-36] Promise -> PromisePrivate

--- std/src/private/promise_p.cpp
+++ std/src/private/promise_p.cpp
@@ -1,66 +1,64 @@
+#include "std/private/promise_p.h"
 #include "std/private/internal_state.h"
-#include "std/private/logger.h"
-#include "std/private/promise.h"
-#include "std/tsobject.h"
 
-Promise::Promise()
+PromisePrivate::PromisePrivate()
     : _internalState{std::make_shared<InternalState>()}
 {
 }
 
-Promise::Promise(const Promise& other)
+PromisePrivate::PromisePrivate(const PromisePrivate& other)
     : _internalState{other._internalState}
 {
 }
 
-Promise Promise::then(Object* onResolved, Object* onRejected)
+PromisePrivate PromisePrivate::then(Object* onResolved, Object* onRejected)
 {
-    Promise next{};
+    PromisePrivate next{};
     _internalState->attach(next, onResolved, onRejected);
     return next;
 }
 
-Promise Promise::then(Object* onResolved)
+PromisePrivate PromisePrivate::then(Object* onResolved)
 {
     return then(onResolved, nullptr);
 }
 
-Promise Promise::then()
+PromisePrivate PromisePrivate::then()
 {
     return then(nullptr, nullptr);
 }
 
-Promise Promise::fail(Object* onRejected)
+PromisePrivate PromisePrivate::fail(Object* onRejected)
 {
     return then(nullptr, onRejected);
 }
 
-Promise Promise::finally(Object* onFinally)
+PromisePrivate PromisePrivate::finally(Object* onFinally)
 {
     return then(onFinally, onFinally);
 }
 
-void Promise::resolve(Object* resolved)
+void PromisePrivate::resolve(Object* resolved)
 {
     _internalState->resolve(InternalState::Result::makeValue(resolved));
 }
 
-void Promise::reject(Object* rejected)
+void PromisePrivate::reject(Object* rejected)
 {
     _internalState->reject(InternalState::Result::makeError(rejected));
 }
 
-bool Promise::ready() const
+bool PromisePrivate::ready() const
 {
     return _internalState->ready();
 }
 
-bool Promise::isFulfilled() const
+bool PromisePrivate::isFulfilled() const
 {
     return _internalState->isFulfilled();
 }
 
-bool Promise::isRejected() const
+bool PromisePrivate::isRejected() const
 {
     return _internalState->isRejected();
 }
