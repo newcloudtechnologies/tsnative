a71e51cd105924af77428f0422aa13252b7b2a34
andrey.tutyne <andrey.tutyne@myoffice.team>
2022-10-20 08:23:13 +0300

    [TSN-38] Run event loop and simplify test without calling promise constructor

--- test/src/compiler/cases/basic/promises.ts
+++ test/src/compiler/cases/basic/promises.ts
@@ -1,19 +1,7 @@
 import {Promise} from "tsnative/std/definitions/tspromise"
 
-type ExecutorCallback = (val: Object) => void;
-
-const p = new Promise((resolve: ExecutorCallback, reject: ExecutorCallback): void => {
-    resolve(23);
+Promise.resolve(true).then((b: boolean) => {
+    console.assert(b);
 });
 
-// const onfuldilled1 = (s: number) => {
-//     console.assert(s === 23);
-//     return 1;
-// };
-//
-// const onRejected1 = (s: number) => {
-//     return 2;
-// }
-//
-// p.then(onfuldilled1, onRejected1);
 
