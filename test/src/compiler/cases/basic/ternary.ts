7c431a3051579c6b56cb72987cfefac4d8744fbc
vladimir.tsyshnatiy <vladimir.tsyshnatiy@myoffice.team>
2022-11-23 10:32:46 +0300

    [TSN-81] Crash due to use of ternary operator
    Чинятся две или три баги.
    Работают:
    https://jira.ncloudtech.ru:8090/browse/TSN-203
    https://jira.ncloudtech.ru:8090/browse/TSN-81
    
    Не проверил, потому что не работают кнопки в нотпаде
    https://jira.ncloudtech.ru:8090/browse/TSN-69
    
    Предполагаю, что тоже должно
    
    Суть проблемы:
    1. Тернарники возвращают union, убедиться можно в ast viewer
    2. На стороне стд, когда делается метод Object::set, то, что пришло как value мы еще раз оборачиваем в юнион.
    3. Когда достаем из объекта - делаем Union::get, но только 1 раз.
    4. Думаем, что там строка, зовем clone на юнионе, тип не тот - упали
    5. В объектных литералах не может быть optional (а у нас раньше могли как раз)
    ![image](/attachments/73aaa286-b2ae-4c57-b282-eda3369d6ac9)
    
    Решение:
    1. Убрать юнионы из литералов, разыменовывая юнион столько раз, сколько он там вложен
    
    Заодно делаем как ванильный ТС
    
    Co-authored-by: Tsyshnatiy <vladimir.tsyshnatiy@myoffice.team>
    Reviewed-on: https://gitea.devos.club/antiq/tsnative/pulls/371
    Reviewed-by: eugene.makarov <eugene.makarov@myoffice.team>
    Reviewed-by: andrey.tutyne <andrey.tutyne@myoffice.team>

--- test/src/compiler/cases/basic/ternary.ts
+++ test/src/compiler/cases/basic/ternary.ts
@@ -9,6 +9,7 @@
  *
  */
 
+// Return User literal
 {
     type FileInfo_t = {
         _type: string,
@@ -20,5 +21,67 @@
         };
     }
 
-    console.assert(lol("folder", "file", false)._type === "file" && lol("folder", "file", true)._type === "folder", "Object property initialized by ternary expression");
+    const message = "Object property is not initialized properly by the ternary expression"
+    console.assert(lol("folder", "file", false)._type === "file" && lol("folder", "file", true)._type === "folder", message);
+}
+
+// Create user object and pass as argument to factory function
+{
+    interface RxButton_args {
+        text: string
+    }
+
+    class RxButton_t  {
+        readonly text: string;
+        constructor(text: string) { 
+            this.text = text;
+        }
+    }
+
+    function RxButton(args: RxButton_args): RxButton_t {
+        console.log(args.text)
+        return new RxButton_t(args.text);
+    }
+
+    let qqq = RxButton({
+        text: true ? "Log In" : "Processing...",
+    })
+
+    console.assert(qqq.text === "Log In", "Ternary: text property is not equal");
+}
+
+// Simple ternary with flag
+{
+    let flag = true;
+    let a = flag ? "a" : "b";
+    console.assert(a === "a", "Ternary: simple ternary is not equal");
+}
+
+// Expression as conditional
+{
+    let a = (1 + 2) ? "a" : "b";
+    console.assert(a === "a", "Ternary: simple ternary is not equal");
+}
+
+// Create user object with ternary and pass directly to the constructor
+{
+    let flag = true;
+
+    interface RxText_args {
+        text: string
+    }
+
+    class RxText {
+        readonly text: string;
+        constructor(args: RxText_args) {
+            this.text = args.text;
+        }
+    }
+
+    let rxText = new RxText(
+        {
+            text: (flag ? "title1" : "title2")
+        });
+
+    console.assert(rxText.text === "title1", "Ternary: text property is not equal");
 }
