e82d18d739159caa8645f1ef84eb85b475b30641
boris.khabarov <Boris.Khabarov@myoffice.team>
2022-11-25 15:20:34 +0300

    TSN-254 remove linux dependent code

--- compiler/private/seed/seed.cpp
+++ compiler/private/seed/seed.cpp
@@ -14,29 +14,6 @@
 
 #include "std/runtime.h"
 
-#include <exception>
-#include <execinfo.h>
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-// TODO remove me
-void handler(int sig)
-{
-    void* array[10];
-    size_t size;
-
-    // get void*'s for all entries on the stack
-    size = backtrace(array, 10);
-
-    // print out all the frames to stderr
-    fprintf(stderr, "Error: signal %d:\n", sig);
-    backtrace_symbols_fd(array, size, STDERR_FILENO);
-
-    std::terminate();
-}
-
 int main(int argc, char* argv[])
 {
     int result = Runtime::init(argc, argv);
@@ -44,9 +21,6 @@ int main(int argc, char* argv[])
     {
         return result;
     }
-
-    // TODO remove me
-    signal(SIGSEGV, handler);
 
     result = __ts_main();
 
