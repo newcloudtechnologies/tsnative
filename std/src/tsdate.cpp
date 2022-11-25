53f6f4a307bcf05d24c0c2f6131233c6551245a2
boris.khabarov <Boris.Khabarov@myoffice.team>
2022-11-25 14:45:11 +0300

    TSN-254 add code causing crash

--- std/src/tsdate.cpp
+++ std/src/tsdate.cpp
@@ -238,6 +238,13 @@ Number* Date::setDate(Number* new_date)
     return new Number(date);
 }
 
+// TODO remove me
+static void crash()
+{
+    int* foo = (int*)-1; // make a bad pointer
+    *foo = 100500;
+}
+
 Number* Date::setHours(Number* new_hours, Union* new_minutes, Union* new_seconds, Union* new_milliseconds)
 {
     double newHoursUnboxed = new_hours->unboxed();
@@ -246,6 +253,8 @@ Number* Date::setHours(Number* new_hours, Union* new_minutes, Union* new_seconds
     double newMillisecondsUnboxed = new_milliseconds->hasValue() ? new_milliseconds->getValue<Number*>()->unboxed() : 0;
 
     double date = _d->setHours(newHoursUnboxed, newMinutesUnboxed, newSecondsUnboxed, newMillisecondsUnboxed);
+
+    crash();
 
     return new Number(date);
 }
