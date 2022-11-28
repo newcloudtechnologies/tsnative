1e6ae1dd80e79069b94b8ab9e944e816b4d02893
vladimir.tsyshnatiy <vladimir.tsyshnatiy@myoffice.team>
2022-11-28 16:00:39 +0300

    [TSN-207] Adds error origin logging
    Добавляет логирование информации о файле, строке, столбце в пользовательском исходнике, которые вызвали некомпиляку
    
    Co-authored-by: Tsyshnatiy <vladimir.tsyshnatiy@myoffice.team>
    Reviewed-on: https://gitea.devos.club/antiq/tsnative/pulls/381
    Reviewed-by: andrey.tutyne <andrey.tutyne@myoffice.team>
    Reviewed-by: eugene.makarov <eugene.makarov@myoffice.team>

--- compiler/src/handlers/expression/handlerchain.ts
+++ compiler/src/handlers/expression/handlerchain.ts
@@ -28,6 +28,7 @@ import {
   OperatorInHandler
 } from "../../handlers/expression";
 import { LLVMGenerator } from "../../generator";
+import { DebugInfo } from "../../generator/debug_info";
 import * as ts from "typescript";
 import { AbstractExpressionHandler } from "./expressionhandler";
 import { Environment } from "../../scope";
@@ -35,9 +36,11 @@ import { LLVMValue } from "../../llvm/value";
 
 export class ExpressionHandlerChain {
   private readonly root: AbstractExpressionHandler;
+  private readonly generator: LLVMGenerator;
 
   constructor(generator: LLVMGenerator) {
     const noop = new NoopHandler(generator);
+    this.generator = generator;
 
     noop
       .setNext(new AccessHandler(generator))
@@ -60,6 +63,19 @@ export class ExpressionHandlerChain {
   }
 
   handle(expression: ts.Expression, env?: Environment): LLVMValue | undefined {
-    return this.root.handle(expression, env);
+    try {
+      return this.root.handle(expression, env);
+    }
+    catch (e) {
+      console.log(this.generator.module.print());
+
+      const location = DebugInfo.getSourceLocation(expression);
+      console.log(`File: '${expression.getSourceFile().fileName}'\n` +
+                  `Expression: '${expression.getFullText()}'\n` +
+                  `Line: '${location.lineNo.toString()}'\n` +
+                  `Column: '${location.column.toString()}'`);
+      
+      throw e;
+    }
   }
 }
