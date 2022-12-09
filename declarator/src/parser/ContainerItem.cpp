ab88243617bc5c1f121cee461f2e501e49b7c19e
dmitriy.kozlov <dmitriy.kozlov@myoffice.team>
2022-12-09 17:47:36 +0300

    TSN-264 Refactor declarator Collection.exists() to avoid empty catch clause (!389)
    Added Collection::find() which returns optional, refactor exists() and
    get() to use find(), mark find as public interface for users, make
    Collection:ref() return const, move main.cpp:printCollection() to
    Collection::print().
    
    Co-authored-by: Dmitriy Kozlov <dmitriy.kozlov@myoffice.team>
    Reviewed-on: https://gitea.devos.club/antiq/tsnative/pulls/389
    Reviewed-by: vladimir.tsyshnatiy <vladimir.tsyshnatiy@myoffice.team>
    Reviewed-by: artem.silivanchik <artem.silivanchik@myoffice.team>

--- declarator/src/parser/ContainerItem.cpp
+++ declarator/src/parser/ContainerItem.cpp
@@ -11,6 +11,8 @@
 
 #include "ContainerItem.h"
 
+#include <algorithm>
+
 namespace parser
 {
 
@@ -23,6 +25,17 @@ ContainerItem::ContainerItem(
 void ContainerItem::addItem(abstract_item_t item)
 {
     m_items.push_back(item);
+}
+
+void ContainerItem::replaceItem(const std::string& name, const std::string& prefix, abstract_item_t new_item)
+{
+    auto it = std::find_if(m_items.begin(),
+                           m_items.end(),
+                           [&name, &prefix](abstract_item_t i) { return name == i->name() && prefix == i->prefix(); });
+    if (it != m_items.end())
+    {
+        *it = new_item;
+    }
 }
 
 item_list_t ContainerItem::children() const
--- declarator/src/parser/ContainerItem.h
+++ declarator/src/parser/ContainerItem.h
@@ -33,6 +33,7 @@ public:
     virtual ~ContainerItem() = default;
 
     void addItem(abstract_item_t item);
+    void replaceItem(const std::string& name, const std::string& prefix, abstract_item_t new_item);
 
     item_list_t children() const;
 };
