80f49cbb3212f1876ffa9017693f0be9c7556aeb
Dmitriy Kozlov <dmitriy.kozlov@myoffice.team>
2022-12-12 21:52:35 +0300

    TSN-85 Fix ignored TS_DECLARE and TS_IGNORE for standalone functions.
    This is part one which fixes only TS_DECLARE.

--- declarator/src/generator/FunctionBlock.h
+++ declarator/src/generator/FunctionBlock.h
@@ -31,17 +31,19 @@ class FunctionBlock : public AbstractBlock
     std::vector<std::string> m_templateArguments;
     std::string m_retType;
     bool m_isExport;
+    bool m_isDeclare;
 
 protected:
     void printBody(generator::print::printer_t printer) const override;
 
 private:
-    FunctionBlock(const std::string& name, const std::string& retType, bool isExport);
+    FunctionBlock(const std::string& name, const std::string& retType, bool isExport, bool isDeclare);
 
 public:
     void addArgument(const std::string& name, const std::string& type, bool isSpread, bool isOptional);
     void addTemplateArgument(const std::string& type);
     bool isExport() const;
+    bool isDeclare() const;
 };
 
 using function_block_t = block_t<FunctionBlock>;
