0fcf8829b75f4f11ad6a9ab3299dcc5150acf539
boris.khabarov <Boris.Khabarov@myoffice.team>
2022-12-13 11:39:32 +0300

    [TSN-166] add crash handler for unix only

--- compiler/seed/seed.cpp
+++ compiler/seed/seed.cpp
@@ -13,6 +13,34 @@
 
 #include "std/runtime.h"
 
+#include <exception>
+#include <execinfo.h>
+#include <iostream>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+// for demangling
+#include <cxxabi.h>
+
+// gcc specific
+void handler(int sig)
+{
+    const int maxSize = 15;
+    void* array[15];
+    size_t size{};
+
+    // get void*'s for all entries on the stack
+    size = backtrace(array, maxSize);
+
+    // print out all the frames to stderr
+    fprintf(stderr, "Error: signal %d:\n", sig);
+    backtrace_symbols_fd(array, size, STDERR_FILENO);
+
+    std::terminate();
+}
+
 int main(int argc, char* argv[])
 {
     int result = Runtime::init(argc, argv);
@@ -20,6 +48,10 @@ int main(int argc, char* argv[])
     {
         return result;
     }
+
+    signal(SIGSEGV, handler);
+    signal(SIGFPE, handler);
+
     result = __ts_main();
 
     return result;
