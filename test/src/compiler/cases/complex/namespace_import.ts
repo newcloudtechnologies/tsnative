ab4ed5b0342a2ac9bdb1886d10102997e1a4faf9
Vladimir Tsyshnatiy <vladimir.tsyshnatiy@myoffice.team>
2023-04-28 17:58:38 +0300

    [TSN-548] Namespaces as global llvm values
    1. Исходная ошибка - неймспейсы не обладали LLVMValue, так как были Scope
    2. Переделал namespace на то, чтобы они были object.
    3. Собрал доки. Мастер и Олину ветку. Все сработало.
    
    Посмотрите плз на тест кейсе сперва. Будет многое понятно в плане реализации в этом случае.
    
    Co-authored-by: Tsyshnatiy <vladimir.tsyshnatiy@myoffice.team>
    Reviewed-on: https://gitea.devos.club/antiq/tsnative/pulls/489
    Reviewed-by: Boris Khabarov <boris.khabarov@myoffice.team>
    Reviewed-by: Eugene Makarov <eugene.makarov@myoffice.team>
    Reviewed-by: Andrey Tutyne <andrey.tutyne@myoffice.team>

---
+++ test/src/compiler/cases/complex/namespace_import.ts
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) New Cloud Technologies, Ltd., 2014-2023
+ *
+ * You can not use the contents of the file in any way without
+ * New Cloud Technologies, Ltd. written permission.
+ *
+ * To obtain such a permit, you should contact New Cloud Technologies, Ltd.
+ * at http://ncloudtech.com/contact.html
+ *
+ */
+
+import { A } from "./namespace_export"
+
+console.assert(A.B.C.foo() === A.B.C.Enum.ENTRY1, "Namespace import failed");
