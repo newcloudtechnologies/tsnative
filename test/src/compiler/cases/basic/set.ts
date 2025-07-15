4673eb5eb03acce5afef87c2e5f407eccecca05f
Golubov Gleb <gleb.golubov@myoffice.team>
2025-07-15 12:09:30 +0300

    EDESK-26640 Removed copyrights (!501)
    Перед публикацией в opensource необходимо удалить копирайты из исходников.
    
    Reviewed-on: https://gitea.devos.club/antiq/tsnative/pulls/501
    Reviewed-by: Артем Силиванчик <artem.silivanchik@myoffice.team>
    Reviewed-by: Дмитрий Жаворонков <dmitriy.zhavoronkov@myoffice.team>
    Co-authored-by: Golubov Gleb <gleb.golubov@myoffice.team>
    Co-committed-by: Golubov Gleb <gleb.golubov@myoffice.team>

--- test/src/compiler/cases/basic/set.ts
+++ test/src/compiler/cases/basic/set.ts
@@ -1,15 +1,3 @@
-/*
- * Copyright (c) New Cloud Technologies, Ltd., 2014-2023
- *
- * You can not use the contents of the file in any way without
- * New Cloud Technologies, Ltd. written permission.
- *
- * To obtain such a permit, you should contact New Cloud Technologies, Ltd.
- * at http://ncloudtech.com/contact.html
- *
- */
-
-
 class A {
     constructor(n: number, s: string) {
         this.n = n;
