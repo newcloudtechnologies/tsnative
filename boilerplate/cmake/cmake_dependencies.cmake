9a33d582533f1ed34bce2fd45e1411990425027a
Golubov Gleb <gleb.golubov@myoffice.team>
2025-07-24 11:36:32 +0300

    removed Copyright from boilerplate

--- boilerplate/cmake/cmake_dependencies.cmake
+++ boilerplate/cmake/cmake_dependencies.cmake
@@ -1,11 +1,1 @@
-#
-# Copyright (c) New Cloud Technologies, Ltd., 2014-2023
-#
-# You can not use the contents of the file in any way without
-# New Cloud Technologies, Ltd. written permission.
-#
-# To obtain such a permit, you should contact New Cloud Technologies, Ltd.
-# at http://ncloudtech.com/contact.html
-#
-
 # put 'find_package' here to make exported package variables available for whole project (e.g. find_package(mgt REQUIRED COMPONENTS ts))
--- boilerplate/cmake/external_extensions.cmake
+++ boilerplate/cmake/external_extensions.cmake
@@ -1,11 +1,1 @@
-#
-# Copyright (c) New Cloud Technologies, Ltd., 2014-2023
-#
-# You can not use the contents of the file in any way without
-# New Cloud Technologies, Ltd. written permission.
-#
-# To obtain such a permit, you should contact New Cloud Technologies, Ltd.
-# at http://ncloudtech.com/contact.html
-#
-
 # put 'add_extension' macro here to include external dependencies in build (e.g. add_extension(MGT::ts))
